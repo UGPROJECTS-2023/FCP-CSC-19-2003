8709f93a8938a0478fd35a2d67de149c67f18a05
abba-makama <149726320+abba-makama@users.noreply.github.com>
2024-01-09 16:10:21 +0100

    Add files via upload
    The Staff ID card generator system is an application that will help generate ID cards in an educational institute in FUD; for ID card generation, there needs to be a proper system.

---
+++ expire.php
@@ -0,0 +1,6 @@
+<?php
+echo "Your system is expire  Please Kindly Contact 08061305081 or send an Email abbagumel111@gmail.com.com";
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+require('dbconn.php');
+?>
+<?php
+
+
+
+include 'private/validity1.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<!-- Head -->
+<head>
+
+	<title>FUD STAFF ID CARD GENERATOR </title>
+
+	<!-- Meta-Tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="keywords" content="fud staff id card Login Form Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- //Meta-Tags -->
+
+	<!-- Style --> <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+	<!-- Fonts -->
+		<link href="//fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+	<!-- //Fonts -->
+
+</head>
+<!-- //Head -->
+
+<!-- Body -->
+<body>
+
+	<h1> FUD STAFF ID CARD GENERATOR</h1>
+
+	<div class="container">
+
+		<div class="login">
+			<h2>Sign In</h2>
+			<form action="index.php" method="post">
+				<input type="text" Name="RollNo" placeholder="ID number" required="">
+				<input type="password" Name="Password" placeholder="Password" required="">
+			
+			
+			<div class="send-button">
+				<!--<form>-->
+					<input type="submit" name="signin" value="Sign In">
+				</form>
+			</div>
+			
+			<div class="clear"></div>
+		</div>
+
+		<div class="clear"></div>
+
+	</div>
+
+	<div class="footer w3layouts agileits">
+		<!--<p> &copy; 2023 FUD </a></p>/!-->
+		
+	</div>
+
+<?php
+if(isset($_POST['signin']))
+{$u=$_POST['RollNo'];
+ $p=$_POST['Password'];
+ $c=$_POST['Category'];
+
+ $sql="select * from LMS.user where RollNo='$u'";
+
+ $result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$x=$row['Password'];
+$y=$row['Type'];
+if(strcasecmp($x,$p)==0 && !empty($u) && !empty($p))
+  {//echo "Login Successful";
+   $_SESSION['RollNo']=$u;
+   
+if ($y == 'Admin') {
+	echo header("Location:admin/index.php");
+
+}elseif($y=='librarian'){
+echo header("Location:admin/index.php");
+
+}elseif ($y=='Student') {
+	echo header("Location:admin/index.php");
+
+}else{
+	echo header('Location:admin/index.php');
+}
+//   if($y=='Admin')
+//   echo header("Location:admin/index.php");
+// elseif ($y == 'librarian') {
+// echo header("Location:librarian/index.php");
+
+// }if ($y =='student') {
+// 	echo header("Location:student/index.php");
+// }
+//   elseif (condition) {
+//   	 echo	header("Location:staff/index.php");
+//   }
+ 
+        
+  }
+else 
+ { echo "<script type='text/javascript'>alert('Failed to Login! Incorrect IDNo or Password')</script>";
+}
+   
+
+}
+
+if(isset($_POST['signup']))
+{
+	$name=$_POST['Name'];
+	$email=$_POST['Email'];
+	$password=$_POST['Password'];
+	$mobno=$_POST['PhoneNumber'];
+	$rollno=$_POST['RollNo'];
+	$category=$_POST['Category'];
+	$department=$_POST['Department'];
+	$type='Student';
+
+	$sql="insert into LMS.user (Name,Type,Category,Department,RollNo,EmailId,MobNo,Password) values ('$name','$type','$category','$department','$rollno','$email','$mobno','$password')";
+
+	if ($conn->query($sql) === TRUE) {
+echo "<script type='text/javascript'>alert('Registration Successful')</script>";
+} else {
+    //echo "Error: " . $sql . "<br>" . $conn->error;
+echo "<script type='text/javascript'>alert('User Exists')</script>";
+}
+}
+
+?>
+
+</body>
+<!-- //Body -->
+
+</html>
+<?php 
+// }
+// else {
+//     echo "<script type='text/javascript'>alert('System Expired')</script>";
+// } 
+
+
+?>
